f0eb68ffc56403a4f899579fc9d1d1f69d6ced1c
Georgescu Rares <raresgeo9@gmail.com>
2021-09-19 07:36:09 +0300

    Added leaderboard

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -16,61 +16,26 @@ module.exports = {
 }; */
 
 module.exports.command = async (message) => {
-  return;
+  let topTen = await User.find({ guild_id: message.guildId })
+    .limit(10)
+    .sort({ points: "desc" });
 
-  let finishReaction = finished;
-  let points = 0;
-  let user;
-  let user_id;
+  let bigField = ['']
+    
+  let promises = topTen.map(async (user, index) => {
+    let _user = await utils.getUser(message, user.user_id);
+    return bigField[index] = `**${index + 1}.** ${_user.tag}  -  💵 ${Math.round((user.points + Number.EPSILON) * 100) / 100}`
+  })
 
-  // Check if there's a mention or a user id in here and run the command for that user instead.
+  await Promise.all(promises)
 
-  // Check for mentions
-  let mentions = message.mentions.members;
-  let commandArgs = utils.parseArgs(message.content)._[0];
-  if (mentions.size) {
-    user = mentions.first().user;
-    user_id = mentions.first().id;
-  } else if (commandArgs && commandArgs.length == 18) {
-    // Check for user id
-    user = await utils.getUser(message, commandArgs);
-    user_id = commandArgs;
-    // TODO: I should check if this user is in the guild, just to make sure people don't add random things to the database
-  } else {
-    user = message.author;
-    user_id = user.id;
-  }
-
-  // Check if user is already in database
-  let _user = await User.findOne({ user_id });
-
-  // If they are not, create a new database and give them 0 points
-  if (!_user) {
-    _user = new User({
-      guild_id,
-      user_id,
-      points,
-      is_active: isActive(newState),
-      is_active_since: Date.now(),
-    });
-
-    try {
-      await _user.save();
-    } catch (err) {
-      console.log(err);
-    }
-  } else {
-    points = _user.points;
-  }
+  bigField = bigField.join('\n')
 
   // Construct embed
   let embed = new MessageEmbed()
     .setColor("DARK_BUT_NOT_BLACK")
-    .setTitle(`${user.username} balance`)
-    .addField("Points: ", `💵 ${points}`)
-    .setTimestamp()
-    .setThumbnail(user.avatarURL());
-  message.channel.send({ embeds: [embed] });
+    .setTitle(`${message.guild.name} leaderboard`)
+    .addField( `Top ${topTen.length}`, bigField);
 
-  utils.react(message, finishReaction);
+  message.channel.send({ embeds: [embed] });
 };
